876d13d4bd8b75f5ed04643d2bc62acd96481182
rishwanth1995 <rishwanthsenthilkumar@hotmail.com>
2018-01-24 13:38:01 -0500

    added sample java application

---
+++ bindings/java/SleuthkitSample/src/sleuthkitsample/SleuthkitSample.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sleuthkitsample;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.UUID;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.sleuthkit.datamodel.AbstractFile;
+import org.sleuthkit.datamodel.Content;
+import org.sleuthkit.datamodel.Image;
+import org.sleuthkit.datamodel.SleuthkitCase;
+import org.sleuthkit.datamodel.SleuthkitJNI.CaseDbHandle.AddImageProcess;
+import org.sleuthkit.datamodel.TskCoreException;
+import org.sleuthkit.datamodel.TskDataException;
+
+/**
+ *
+ * @author rishwanth03
+ */
+public class SleuthkitSample {
+public static void run(String imagePath) {
+		try {
+			SleuthkitCase sk = SleuthkitCase.newCase(imagePath + ".db");
+
+			// initialize the case with an image
+			String timezone = "";
+			AddImageProcess process = sk.makeAddImageProcess(timezone, true, false, "");
+			ArrayList<String> paths = new ArrayList<String>();
+			paths.add(imagePath);
+			try {
+				process.run(UUID.randomUUID().toString(), paths.toArray(new String[paths.size()]));
+			} catch (TskDataException ex) {
+				Logger.getLogger(SleuthkitSample.class.getName()).log(Level.SEVERE, null, ex);
+			}
+			process.commit();
+
+			// print out all the images found, and their children
+			List<Image> images = sk.getImages();
+			for (Image image : images) {
+				System.out.println("Found image: " + image.getName());
+				System.out.println("There are " + image.getChildren().size() + " children.");
+				for (Content content : image.getChildren()) {
+					System.out.println('"' + content.getName() + '"' + " is a child of " + image.getName());
+				}
+			}
+
+			// print out all .txt files found
+			List<AbstractFile> files = sk.findAllFilesWhere("LOWER(name) LIKE LOWER('%.txt')");
+			for (AbstractFile file : files) {
+				System.out.println("Found text file: " + file.getName());
+			}
+
+		} catch (TskCoreException e) {
+			System.out.println("Exception caught: " + e.getMessage());
+			SleuthkitSample.usage(e.getMessage());
+
+		}
+	}
+
+	public static void usage(String error) {
+		System.out.println("Usage: ant -Dimage:{image string} run-sample");
+		if (error.contains("deleted first")) {
+			System.out.println("A database for the image already exists. Delete it to run this sample again.");
+		} else if (error.contains("unable to open database")) {
+			System.out.println("Image must be encapsulated by double quotes. Ex: ant -Dimage=\"C:\\Users\\You\\image.E01\" run-sample");
+		}
+	}
+
+	public static void main(String[] args) {
+                System.out.println("Enter the imagefile path");
+                Scanner scanner = new Scanner(System.in);
+                String path = scanner.nextLine();
+		run(path);
+	}
+
+}
