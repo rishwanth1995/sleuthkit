1543f60c8c5b2913c38dd71ee62841425d711137
Rishwanth Senthilkumar <rishwanth@basistech.com>
2018-08-16 12:18:22 -0400

    Update travis_build.sh with tar file

--- travis_build.sh
+++ travis_build.sh
@@ -1,11 +1,9 @@
 #!/bin/sh
 set -ex
 installLib() {
-	git clone https://github.com/libyal/$1
-	cd $1
-	git checkout tags/$2
-	./synclibs.sh
-	./autogen.sh
+	wget https://github.com/libyal/libvhdi/releases/download/$2/$1-alpha-$2.tar.gz
+	tar -xzf libvhdi-alpha-$1.tar.gz
+	cd $1-$2
 	if test ${TRAVIS_OS_NAME} = "linux"; then
 		./configure -prefix=/usr > /dev/null
 	else
@@ -17,10 +15,10 @@ installLib() {
 
 if test ${TRAVIS_OS_NAME} = "linux"; then
 	sudo apt-get -qq update
-	sudo apt-get -y install libafflib-dev libewf-dev libpq-dev autopoint libsqlite3-dev ant libcppunit-dev
+	sudo apt-get -y install libafflib-dev libewf-dev libpq-dev autopoint libsqlite3-dev ant libcppunit-dev wget
 elif test ${TRAVIS_OS_NAME} = "osx"; then
 	export PATH=${PATH}:/usr/local/opt/gettext/bin
-	brew install ant libewf gettext cppunit
+	brew install ant libewf gettext cppunit wget
 fi
 installLib libvhdi 20160424
 installLib libvmdk 20160119
